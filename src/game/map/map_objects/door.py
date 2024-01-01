87f40c795993882f9c981bdcf7c24e79f0078af4
dctime <daniel.chu916@gmail.com>
2024-01-01 18:45:42 +0800

    Feature: making the door

--- src/game/map/map_objects/door.py
+++ src/game/map/map_objects/door.py
@@ -1,2 +1,21 @@
 class Door():
-    print("hello door")
+    """
+    Well its a door
+
+    Attributes:
+        __is_open: true when door is in open state. False when door is closed.
+        __is_broken: true when door flew away, false when door is still there.
+
+    Methods:
+        use: open/close the door
+        break_it: break the door
+    """
+    def __init__(self):
+        self.__is_open = False
+        self.__is_broken = False
+
+    def use(self):
+        self.__is_open = not self.__is_open
+
+    def break_it(self):
+        self.__is_broken = True
