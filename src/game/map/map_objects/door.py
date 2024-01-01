efc5c12bd74265b0623e6e68b94055ab08ba16dd
dctime <daniel.chu916@gmail.com>
2024-01-01 18:49:22 +0800

    Feature: Adding som docstring to the Door class while making the brken door logic better

--- src/game/map/map_objects/door.py
+++ src/game/map/map_objects/door.py
@@ -15,7 +15,18 @@ class Door():
         self.__is_broken = False
 
     def use(self):
-        self.__is_open = not self.__is_open
+        """
+        open/close the door. If the door is open, close the door. If the door is closed, open the door.
+        """
+        if (self.__is_broken == False):
+            self.__is_open = not self.__is_open
+        else:
+            self.__is_open = True
 
     def break_it(self):
+        """
+        break the door. If the door is still there, break the door. If door is dead, then do nothing.
+        the door will turn into closed state and cannot be open because um...
+        """
         self.__is_broken = True
+        self.__is_open = True
