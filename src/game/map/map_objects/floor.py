5a7c9d0ec83f1f6dd1eaeb7fd24df680cf120ead
dctime <daniel.chu916@gmail.com>
2024-01-01 18:57:24 +0800

    Feature: adding location to the floor

--- src/game/map/map_objects/floor.py
+++ src/game/map/map_objects/floor.py
@@ -1,6 +1,15 @@
 class Floor():
     """
     Its just a floor
+
+    Attributes:
+        __location (tuple): the location of the floor
+
+    Methods:
+        get_location: the getter of the __location attribute
     """
-    def __init__():
-        pass
+    def __init__(self, location:tuple):
+        self.__location = location
+
+    def get_location(self):
+        return self.__location
