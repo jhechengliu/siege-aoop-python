eca7a0ca19a4a18e8ceea8ca94bf2ef77755273e
JeremyLiu <jeremyliu0933@gmail.com>
2024-01-11 20:55:26 +0800

    My name is JL.

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from game.player import Player
 logging.basicConfig(level=logging.INFO)
 
 defend_player = Player("DCtime")
-attack_player = Player("Hmm")
+attack_player = Player("JL")
 builder = game.map_builder.MapBuilder.get_instance("map_example", defend_player, attack_player)
 map = builder.get_map()
 map.print_map()
