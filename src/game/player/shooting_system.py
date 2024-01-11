b52fd059eba3b657f10b581b728328e06fd065bb
JeremyLiu <jeremyliu0933@gmail.com>
2024-01-11 10:57:15 +0800

    Add class 'ShootingSystem'. Wrote docstring.

---
+++ src/game/player/shooting_system.py
@@ -0,0 +1,16 @@
+from player import operator
+import random
+
+class ShootingSystem:
+    """
+    This class represents the shooting system in the game.
+    It contains the shooting mechanics.
+
+    Attributes:
+        accuracy (int): The accuracy of the operator. Depends on the mode of the operator.
+        damage (int): The damage done to the target operator. Randomly generated.
+
+    Methods:
+        aim(operator: Operator) -> None: Base on the mode of the operator, the accuracy of the operator changes.
+        shoot(operator: Operator) -> None: Calculates the damage done to the target operator, depending on the accuracy of the operator.
+    """
