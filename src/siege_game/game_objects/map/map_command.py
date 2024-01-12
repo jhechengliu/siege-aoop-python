97690a2124995c4067e72a33263d96f7c949f999
dctime <daniel.chu916@gmail.com>
2024-01-12 17:08:10 +0800

    Feature: MapCommand abstract class add stuff

--- src/siege_game/game_objects/map/map_command.py
+++ src/siege_game/game_objects/map/map_command.py
@@ -1,12 +1,22 @@
 from abc import ABC
 import abc
 from siege_game.game_objects.states.state import State
+from siege_game.game_objects.map.map import Map
+from siege_game.game_objects.player import Player
 import typing
 
 class MapCommand(ABC):
+    def __init__(self, sender:Player):
+        self.__map:Map = Map.get_instance()
+        self.__sender:Player = sender
+
     @abc.abstractmethod
     def execute(self, check:typing.Callable) -> bool:
-        raise NotImplementedError()
+        if check():
+            return True
+        
+        else:
+            return False
     
     @abc.abstractclassmethod
     def check(cls) -> bool: 
