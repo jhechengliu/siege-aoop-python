bbaf53437303c8e0aa99a16babcdcc06e186820b
JeremyLiu <jeremyliu0933@gmail.com>
2024-01-15 15:23:55 +0800

    Feature: 'PlayerMovementBattleCommand'.

--- src/siege_game/game_objects/map/commands/battle.py
+++ src/siege_game/game_objects/map/commands/battle.py
@@ -1,4 +1,11 @@
 from siege_game.game_objects.map.commands.map_command import MapCommand
 import logging
 
+class PlayerMovementBattleCommand(MapCommand):
+    logging = logging.getLogger("PlayerMovementBattleCommand")
 
+    def execute(self) -> None:
+        pass
+    
+    def check(self) -> bool:
+        pass
