216f2e67aa5c159b4c17d7fe460c2928c39f1fa4
dctime <daniel.chu916@gmail.com>
2024-01-16 21:13:48 +0800

    Fix: bruh

--- catkin_ws/src/example/src/siege_game/game_objects/logger.py
+++ catkin_ws/src/example/src/siege_game/game_objects/logger.py
@@ -5,16 +5,16 @@ class Logger():
         self.__name = f"[{name}]"
 
     def info(self, msg, *args, **kwargs):
-        rospy.loginfo(f"[{self.__name}]", msg, *args, **kwargs)
+        rospy.loginfo(self.__name, msg, *args, **kwargs)
 
     def error(self, msg, *args, **kwargs):
-        rospy.logerr(f"[{self.__name}]", msg, *args, **kwargs)
+        rospy.logerr(self.__name, msg, *args, **kwargs)
 
     def debug(self, msg, *args, **kwargs):
-        rospy.logdebug(f"[{self.__name}]", msg, *args, **kwargs)
+        rospy.logdebug(self.__name, msg, *args, **kwargs)
 
     def warn(self, msg, *args, **kwargs):
-        rospy.logwarn(f"[{self.__name}]", msg, *args, **kwargs)
+        rospy.logwarn(self.__name, msg, *args, **kwargs)
 
     def fatal(self, msg, *args, **kwargs):
-        rospy.logfatal(f"[{self.__name}]", msg, *args, **kwargs)
+        rospy.logfatal(self.__name, msg, *args, **kwargs)
