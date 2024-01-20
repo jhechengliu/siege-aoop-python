90ce5c489a0a94f17026474a8169cd18ecba8598
JeremyLiu <jeremyliu0933@gmail.com>
2024-01-20 12:03:39 +0800

    Move test into main

--- catkin_ws/src/example/src/test_game.py
+++ catkin_ws/src/example/src/test_game.py
@@ -2,12 +2,12 @@
 import pytest
 from src.main import Game
 
-class TestGame():
-    def setup_method(self):
-        self.game = Game("test_game")
+# class TestGame():
+#     def setup_method(self):
+#         self.game = Game("test_game")
     
-    def test_get_commander(self):
-        assert self.game.get_commander() == None
+#     def test_get_commander(self):
+#         assert self.game.get_commander() == None
     
-    def test_get_map(self):
-        assert self.game.get_map() == None
+#     def test_get_map(self):
+#         assert self.game.get_map() == None
