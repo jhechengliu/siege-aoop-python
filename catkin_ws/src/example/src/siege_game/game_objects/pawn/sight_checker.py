e84533bcdff0e72616b34b26bee7f90b4c7941fb
JeremyLiu <jeremyliu0933@gmail.com>
2024-01-20 20:58:35 +0800

    fix () issue

--- catkin_ws/src/example/src/siege_game/game_objects/pawn/sight_checker.py
+++ catkin_ws/src/example/src/siege_game/game_objects/pawn/sight_checker.py
@@ -16,7 +16,7 @@ class SightChecker:
 
         for i in range(0, 1000):
             point = point[0] + delta_x, point[1] + delta_y
-            if (self.__map_data[point].get_is_transparent() == False):
+            if ((self.__map_data[point]).get_is_transparent() == False):
                 return False
         
         return True
