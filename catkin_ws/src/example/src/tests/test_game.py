cbf442253ef582549cdabf8d67ea28b4e22b6eca
jeremy <jeremyliu0933@gmail.com>
2024-01-20 12:59:22 +0800

    Fix: YEAH

--- catkin_ws/src/example/src/tests/test_game.py
+++ catkin_ws/src/example/src/tests/test_game.py
@@ -1,12 +1,11 @@
 import pytest
 from  siege_game.game import Game
 
-# class TestGame():
-#     def setup_method(self):
-#         self.game = Game("test_game")
+class TestGame():
+    def setup_method(self):
+        self.game = Game("test_game")
     
-#     def test_get_commander(self):
-#         assert self.game.get_commander() == None
-    
-#     def test_get_map(self):
-#         assert self.game.get_map() == None
+    def test_get_commander(self):
+        assert self.game.get_commander() == None
+    def test_get_map(self):
+        assert self.game.get_map() == None
