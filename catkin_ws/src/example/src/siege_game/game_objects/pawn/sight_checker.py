e2225e2b23d6617f8080da92f6391882bf2257ad
JeremyLiu <jeremyliu0933@gmail.com>
2024-01-20 21:34:27 +0800

    keep trying

--- catkin_ws/src/example/src/siege_game/game_objects/pawn/sight_checker.py
+++ catkin_ws/src/example/src/siege_game/game_objects/pawn/sight_checker.py
@@ -11,11 +11,11 @@ class SightChecker:
         self.__map_data = map_data
         start_x = location_b[0] - location_a[0]
         start_y = location_b[1] - location_a[1]
-        delta_x = (location_b[0] - location_a[0]) / 1000
-        delta_y = (location_b[1] - location_a[1]) / 1000
+        delta_x = (location_b[0] - location_a[0]) / 100
+        delta_y = (location_b[1] - location_a[1]) / 100
         point = start_x, start_y
 
-        for i in range(0, 1000):
+        for i in range(0, 100):
             point = point[0] + delta_x, point[1] + delta_y
             if ((self.__map_data[ ( math.floor(point[0]), math.floor(point[1]) ) ]).get_is_transparent() == False):
                 return False
--- catkin_ws/src/example/src/tests/sight_checker_test.py
+++ catkin_ws/src/example/src/tests/sight_checker_test.py
@@ -11,7 +11,7 @@ def test_sight_checker():
 
     assert type(map_data) == dict
     assert sight_checker.check_sight(map_data, [0,0], [0,0]) == False
-    assert sight_checker.check_sight(map_data, [0,0], [0,1]) == False
     assert sight_checker.check_sight(map_data, [1,1], [1,2]) == True
     assert sight_checker.check_sight(map_data, [1,1], [8,8]) == False
     assert sight_checker.check_sight(map_data, [1.5,1.7], [3.3, 6.6]) == False
+    assert sight_checker.check_sight(map_data, [0,0], [0,1]) == False
