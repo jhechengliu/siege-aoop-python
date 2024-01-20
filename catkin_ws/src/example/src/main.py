de509dd6dd664bfde2c957464e0be5a73ddd4f85
JeremyLiu <jeremyliu0933@gmail.com>
2024-01-20 13:06:34 +0800

    Try keep on track

--- catkin_ws/src/example/src/main.py
+++ catkin_ws/src/example/src/main.py
@@ -37,14 +37,3 @@ if __name__ == "__main__":
         del thread1
         del thread2
         del logger
-
-    
-class TestGame():
-    def setup_method(self):
-        self.game = Game("test_game")
-    
-    def test_get_commander(self):
-        assert self.game.get_commander() == None
-    
-    def test_get_map(self):
-        assert self.game.get_map() == None
--- catkin_ws/src/example/src/tests/test_game.py
+++ catkin_ws/src/example/src/tests/test_game.py
@@ -3,7 +3,7 @@ from  siege_game.game import Game
 
 class TestGame():
     def setup_method(self):
-        self.game = Game("test_game")
+        self.game = Game()
     
     def test_get_commander(self):
         assert self.game.get_commander() == None
