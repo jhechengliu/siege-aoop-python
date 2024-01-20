10fc80f9fba479f46671a50c32e472b4267b67e4
JeremyLiu <jeremyliu0933@gmail.com>
2024-01-20 21:26:25 +0800

    location is tuple

--- catkin_ws/src/example/src/tests/sight_checker_test.py
+++ catkin_ws/src/example/src/tests/sight_checker_test.py
@@ -9,6 +9,7 @@ def test_sight_checker():
     sight_checker = SightChecker()
     map_data = (testing_map.get_map()).get_map_data()
 
+    assert type(map_data) == dict
     assert sight_checker.check_sight(map_data, [0,0], [0,0]) == False
     assert sight_checker.check_sight(map_data, [0,0], [0,1]) == False
     assert sight_checker.check_sight(map_data, [1,1], [1,2]) == True
