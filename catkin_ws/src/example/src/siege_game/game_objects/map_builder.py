949608811449a4058071fff091858986d97ec6e7
JeremyLiu <jeremyliu0933@gmail.com>
2024-01-20 18:51:15 +0800

    Fix path

--- catkin_ws/src/example/src/siege_game/game_objects/map_builder.py
+++ catkin_ws/src/example/src/siege_game/game_objects/map_builder.py
@@ -42,7 +42,7 @@ class MapBuilder:
         Returns:
             (None)
         """
-        with open('././Assets/Data/' + file_name + '.json', 'r') as json_file:
+        with open('/Assets/Data/' + file_name + '.json', 'r') as json_file:
             # Load the JSON content into a Python dictionary
             self.__map_json = json.load(json_file)
             MapBuilder.logger.info("Raw map file loaded")
