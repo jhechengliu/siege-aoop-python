141b4093ca8b0bd3248a114d27b9f6c5c7ad1478
JeremyLiu <jeremyliu0933@gmail.com>
2024-01-21 22:14:50 +0800

    Fix: __init__

--- catkin_ws/src/example/src/siege_game/game_objects/pawn/defender.py
+++ catkin_ws/src/example/src/siege_game/game_objects/pawn/defender.py
@@ -3,9 +3,12 @@ from siege_game.game_objects.constants.identity import Identity
 
 class Defender(Operator):
 
-    def __init__(self, name:str, location:tuple, health:int, attack:int, defense:int, speed:int, mode:str) -> None:
-        super().__init__(name, location, health, attack, defense, speed, mode)
+    def __init__(self, location:list) -> None:
+        super().__init__(location)
         self.__identity = Identity.DEFEND
+
+    def get_identity(self) -> Identity:
+        return self.__identity
     
     def __str__(self):
         return f"Defender ({self.get_location()})"
