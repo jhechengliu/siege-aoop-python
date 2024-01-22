0c47104128d6833574f4ee910336be2df68298bb
dctime <daniel.chu916@gmail.com>
2024-01-22 11:54:29 +0800

    debug mode

--- catkin_ws/src/example/src/main.py
+++ catkin_ws/src/example/src/main.py
@@ -11,7 +11,7 @@ from siege_game.game_objects.logger import Logger
 
 if __name__ == "__main__":
     while True:
-        rospy.init_node('cube_position_node', log_level=rospy.FATAL)
+        rospy.init_node('cube_position_node', log_level=rospy.DEBUG)
         logging.basicConfig(level=logging.NOTSET)
         logger = Logger("main")
         logger.debug("Start server...")
