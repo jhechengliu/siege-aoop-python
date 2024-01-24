3e7b545a5d32847ff11666975bdee5209ac3af49
dctime <daniel.chu916@gmail.com>
2024-01-24 17:27:34 +0800

    Fix: unittest

--- catkin_ws/src/example/src/tests/test_game_invoker.py
+++ catkin_ws/src/example/src/tests/test_game_invoker.py
@@ -6,14 +6,17 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 
 from siege_game.game_objects.game_invoker import GameInvoker
 from siege_game.game_objects.player import Player
+from siege_game.game_objects.constants.identity import Identity
+from siege_game.game_objects.commander import Commander
+from siege_game.game import Game
 
 class TestGameInvoker(unittest.TestCase):
     def setUp(self):
         self.game_invoker = GameInvoker(shortuuid.uuid())
 
     def test_get_client_A_player(self):
-        player_A = Player("Alice", "A", "CommanderA")
-        self.game_invoker.set_client_A_player(player_A)
+        new_uuid = shortuuid.uuid()
+        player_A = Player("Alice", Identity.ATTACK, Commander(Game(shortuuid.uuid(new_uuid), GameInvoker(new_uuid))))
         self.assertEqual(self.game_invoker.get_client_A_player(), player_A)
 
 if __name__ == '__main__':
